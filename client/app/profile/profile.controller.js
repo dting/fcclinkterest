ae096b9ece0b3b5d8e9abea759804f068d6bd8a3
Dennis Ting <dennisting@gmail.com>
2015-07-22 15:43:08 -0700

    Added profile route.

---
+++ client/app/profile/profile.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('fcclinkterestApp')
+  .controller('ProfileCtrl', function ($scope) {
+    $scope.message = 'Hello';
+  });
---
+++ client/app/profile/profile.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('fcclinkterestApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('profile', {
+        url: '/profile',
+        templateUrl: 'app/profile/profile.html',
+        controller: 'ProfileCtrl'
+      });
+  });
