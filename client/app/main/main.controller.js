f0c98e4b168353df413c0a4ba206a3d14eecb91a
Dennis Ting <dennisting@gmail.com>
2015-07-29 05:33:44 -0700

    Change lightbox appearance.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,33 +1,36 @@
 'use strict';
 
 angular.module('fcclinkterestApp').controller('MainCtrl',
-    function($scope, Auth, User, $state) {
-      $scope.link = {};
-      Auth.isLoggedInAsync(function(isLoggedIn) {
-        $scope.links = isLoggedIn ? Auth.getCurrentUser().links : undefined;
-      });
-      $scope.isLoggedIn = Auth.isLoggedIn;
-      $scope.loginOauth = Auth.login;
-
-      $scope.add = function() {
-        if ($scope.link.url) {
-          User.add({}, {url: $scope.link.url}, function(res) {
-            $scope.link.url = '';
-            $scope.links.push(res);
-          });
-        }
-      };
-
-      $scope.remove = function(link) {
-        console.log(link);
-        User.remove({}, link).$promise.then(function() {
-          _.remove($scope.links, link);
-        });
-      };
-
-      $scope.random = function() {
-        User.random({}, function(res) {
-          $state.go('profile', {id: res._id});
-        });
-      };
+  function($scope, Auth, User, $state) {
+    $scope.link = {};
+    Auth.isLoggedInAsync(function(isLoggedIn) {
+      $scope.links = isLoggedIn ? Auth.getCurrentUser().links : undefined;
     });
+    $scope.isLoggedIn = Auth.isLoggedIn;
+    $scope.loginOauth = Auth.login;
+
+    $scope.add = function() {
+      if ($scope.link.url) {
+        User.add({}, {url: $scope.link.url}, function(res) {
+          $scope.link.url = '';
+          $scope.links.push(res);
+        }, function(err) {
+          console.log('Error adding link', err, $scope.link.url);
+        });
+      }
+    };
+
+    $scope.remove = function(link) {
+      User.remove({}, link, function() {
+        _.remove($scope.links, link);
+      }, function(err) {
+        console.log('Error removing link', err, link);
+      });
+    };
+
+    $scope.random = function() {
+      User.random({}, function(res) {
+        $state.go('profile', {id: res._id});
+      });
+    };
+  });
--- client/app/profile/profile.controller.js
+++ client/app/profile/profile.controller.js
@@ -3,14 +3,18 @@
 var app = angular.module('fcclinkterestApp');
 
 app.config(function(LightboxProvider) {
+  LightboxProvider.templateUrl = '/app/profile/lightbox-template.html';
   LightboxProvider.getImageUrl = function(image) {
+    return image.url;
+  };
+  LightboxProvider.getImageCaption = function (image) {
     return image.url;
   };
 });
 
-app.controller('ProfileCtrl', function($scope, $stateParams, $http,
-    Lightbox, $window) {
-  $http.get('/api/users/profile/' + $stateParams.id)
+app.controller('ProfileCtrl',
+  function($scope, $stateParams, $http, Lightbox, $window) {
+    $http.get('/api/users/profile/' + $stateParams.id)
       .success(function(user) {
         $scope.userName = user.name;
         $scope.links = user.links;
@@ -18,18 +22,18 @@ app.controller('ProfileCtrl', function($scope, $stateParams, $http,
         $scope.notFound = true;
       });
 
-  $scope.openLightboxModal = function(index) {
-    $scope.m = Lightbox.openModal($scope.links, index);
-  };
+    $scope.openLightboxModal = function(index) {
+      $scope.m = Lightbox.openModal($scope.links, index);
+    };
 
-  var w = angular.element($window).bind('resize', function () {
-    if ($scope.m) {
-      $scope.m.close();
-      $scope.m = null;
-    }
-  });
+    var w = angular.element($window).bind('resize', function() {
+      if ($scope.m) {
+        $scope.m.close();
+        $scope.m = null;
+      }
+    });
 
-  $scope.$on('$destroy', function() {
-    w.unbind('resize');
+    $scope.$on('$destroy', function() {
+      w.unbind('resize');
+    });
   });
-});
